599dae1141ae1a0fe7d6abcb6ae99e18bede5593
𝕰𝖝𝖈𝖆𝖑𝖎𝖇𝖗𝖆 <83846602+Excalibra@users.noreply.github.com>
2026-03-28 06:18:49 +0300

    Update setup.py

--- Linux/setup.py
+++ Linux/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='annefrank_injector',    
     version='1.0',
-    description='Windows shellcode loader with persistence features (fork of CTFPacker)',
+    description='Windows shellcode loader with persistence features',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Excalibra/AnneFrankInjector', 
